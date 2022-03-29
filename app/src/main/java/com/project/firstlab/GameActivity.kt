17f4b3629f36bc4e689d89d59656b56f8d36712f
Leshachok <leshak1207@gmail.com>
2022-03-29 21:43:50 +0300

    not first commit

--- app/src/main/java/com/project/firstlab/GameActivity.kt
+++ app/src/main/java/com/project/firstlab/GameActivity.kt
@@ -49,6 +49,7 @@ class GameActivity : AppCompatActivity() {
     }
 
     fun nextWord(){
+        if(index == 10) setContentView(R.layout.activity_game_win)
         currentWord = words[index]
         shuffledWord =  String(currentWord.toCharArray().apply { shuffle() })
 
